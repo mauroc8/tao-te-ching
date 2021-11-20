ab409c3b60360fd4742a42136b8d179dc9b537c7
Mauro Cano <maurocanobrusa@gmail.com>
2021-11-20 00:01:26 -0300

    New build with fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf docs
 mkdir docs
-LANGUAGE="English StephenMitchell" parcel build src/index.html --public-url ./ --out-dir docs/
-LANGUAGE="English AddissAndLombardo" parcel build src/index.html --public-url ./ --out-dir docs/addiss-lombardo/
-LANGUAGE="Spanish" parcel build src/index.html --public-url ./ --out-dir docs/es/
+LANGUAGE="English StephenMitchell" npx parcel build src/index.html --public-url ./ --out-dir docs/
+LANGUAGE="English AddissAndLombardo" npx parcel build src/index.html --public-url ./ --out-dir docs/addiss-lombardo/
+LANGUAGE="Spanish" npx parcel build src/index.html --public-url ./ --out-dir docs/es/
